c5add1a7c34c9d9f0089f659fd4d7042a74d36be
Memester <mattias.henriksson@elev.cybergymnasiet.se>
2017-05-30 11:40:12 +0200

    Add files via upload

---
+++ inlamning_mattiasH/index.js
@@ -0,0 +1,25 @@
+function hoverObjects {
+    .middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%)
+}
+
+.card:hover .image {
+  opacity: 0.3;
+}
+
+.card:hover .middle {
+  opacity: 1;
+}
+
+.text {
+  background-color: #4CAF50;
+  color: white;
+  font-size: 16px;
+  padding: 16px 32px;
+}
